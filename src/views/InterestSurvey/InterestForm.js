9d3f9725dd31633548490536bcd30a716b9a588d
KHAIWAN <khai_chiao@outlook.co.th>
2023-12-26 18:10:50 +0700

    add page
    page Interest Survey

---
+++ src/views/InterestSurvey/InterestForm.js
@@ -0,0 +1,286 @@
+// ** react imports
+import React, { useEffect, useState } from 'react'
+
+// ** mui components
+import {
+  Box,
+  Button,
+  Card,
+  FormControl,
+  FormControlLabel,
+  Radio,
+  RadioGroup,
+  Typography,
+  CircularProgress,
+  Grid,
+  CardContent,
+  Hidden
+} from '@mui/material'
+
+// ** axios
+import axios from 'axios'
+
+const InterestForm = ({ dataSurvey, collegianCode, setAlertAnswer, setResultDisplay }) => {
+  const Questions = dataSurvey[0].interestQuestions
+  const [isLoading, setIsLoading] = useState(false)
+
+  const [answers, setAnswers] = useState(
+    Questions.map(question => ({
+      collegian_code: collegianCode,
+      question_type: question.interest_question_type,
+      interest_answer_id: null,
+      interest_question_score: null
+    }))
+  )
+
+  const handleInputChange = (event, index, type, answerId) => {
+    if (type === 1) {
+      console.log('index: ', index)
+      console.log('answerId: ', answerId)
+      console.log('value: ', event.target.value)
+
+      // const answerId = answerId[0].interest_answer_id
+      const value = parseInt(event.target.value, 10)
+      setAnswers(prevAnswers => {
+        const newAnswers = [...prevAnswers]
+        newAnswers[index] = {
+          ...newAnswers[index],
+          interest_question_score: value,
+          interest_answer_id: answerId
+        }
+
+        return newAnswers
+      })
+    } else if (type === 2) {
+      const value = parseInt(event.target.value, 10)
+      setAnswers(prevAnswers => {
+        const newAnswers = [...prevAnswers]
+        newAnswers[index] = { ...newAnswers[index], interest_answer_id: value }
+
+        return newAnswers
+      })
+    }
+  }
+
+  const handleSubmit = async event => {
+    event.preventDefault()
+
+    // ตรวจสอบเงื่อนไข
+    if (
+      answers.some(answer => {
+        if (answer.question_type === 1) {
+          return answer.interest_question_score === null
+        } else if (answer.question_type === 2) {
+          return answer.interest_answer_id === null
+        }
+
+        return false // ในกรณีที่ไม่ใช่ question_type ที่กำหนด
+      })
+    ) {
+      setAlertAnswer(true)
+    } else {
+      setIsLoading(true)
+
+      const payload = {
+        interest_records: answers
+      }
+
+      try {
+        // ทำการ POST ไปยัง API
+        const res = await axios.post(`${process.env.NEXT_PUBLIC_API_URL}interest-records`, payload, {
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        })
+
+        console.log(res)
+        setResultDisplay(true)
+      } catch (err) {
+        console.log(err)
+        setResultDisplay(false)
+      }
+
+      console.log(answers)
+    }
+  }
+
+  useEffect(() => {
+    console.log('answers: ', answers)
+  }, [answers])
+
+  if (isLoading) {
+    return (
+      <Box sx={{ display: 'flex', justifyContent: 'center' }}>
+        <CircularProgress />
+      </Box>
+    )
+  }
+
+  return (
+    <Box sx={{ paddingX: { xs: 10, md: 24 } }}>
+      <Grid container spacing={6}>
+        <Grid item xs={12}>
+          <Grid container spacing={4} direction={{ xs: 'column-reverse', md: 'row' }}>
+            <Grid item xs={12} md={8}>
+              <Box sx={{ paddingTop: 10 }}>
+                <Typography variant='h5' sx={{ fontWeight: 'bold' }}>
+                  Interest Survey Study Plan System
+                </Typography>
+              </Box>
+            </Grid>
+            <Grid item xs={12} md={4}>
+              <Box
+                sx={{
+                  backgroundColor: '#000',
+                  height: '69px',
+                  borderBottomLeftRadius: '12px',
+                  borderBottomRightRadius: '12px'
+                }}
+              >
+                <CardContent sx={{ display: 'flex', justifyContent: 'center' }}>
+                  <Typography variant='subtitle1' sx={{ fontWeight: 'bold', color: '#7A7A7A' }}>
+                    #{collegianCode}
+                  </Typography>
+                  <Typography variant='subtitle1' sx={{ fontWeight: 'bold', color: '#FFF' }}>
+                    {''}Profile name
+                  </Typography>
+                </CardContent>
+              </Box>
+            </Grid>
+          </Grid>
+        </Grid>
+        <Grid item xs={12}>
+          <Grid container spacing={4}>
+            <Grid item xs={12} md={8}>
+              <Box>
+                <form onSubmit={handleSubmit}>
+                  {Questions.map((question, index) => (
+                    <Card key={index} sx={{ marginBottom: 4 }}>
+                      <CardContent>
+                        <Typography variant='h6' sx={{ fontWeight: 'bold' }}>
+                          {index + 1}
+                          {'). '} {question.interest_question_title}
+                        </Typography>
+                        <FormControl key={index} sx={{ marginBlock: 2 }}>
+                          {question.interest_question_type === 1 ? (
+                            <RadioGroup
+                              row
+                              value={answers[index].interest_question_score}
+                              onChange={event =>
+                                handleInputChange(event, index, 1, question.interest_answers[0].interest_answer_id)
+                              }
+                            >
+                              {[1, 2, 3, 4, 5].map(answer => (
+                                <FormControlLabel
+                                  key={answer}
+                                  value={answer}
+                                  control={<Radio />}
+                                  label={answer}
+                                  sx={{ fontWeight: 'bold' }}
+                                />
+                              ))}
+                            </RadioGroup>
+                          ) : (
+                            <RadioGroup
+                              aria-labelledby='demo-controlled-radio-buttons-group'
+                              name='controlled-radio-buttons-group'
+                              value={answers[index].interest_answer_id}
+                              onChange={event => handleInputChange(event, index, 2)}
+                            >
+                              {question.interest_answers.map((answer, index) => (
+                                <FormControlLabel
+                                  key={answer.interest_answer_id}
+                                  value={answer.interest_answer_id}
+                                  control={<Radio />}
+                                  label={<Typography variant='body1'>{answer.interest_answer_title}</Typography>}
+                                  sx={{ marginBlock: 0.5 }}
+                                />
+                              ))}
+                            </RadioGroup>
+                          )}
+                        </FormControl>
+                      </CardContent>
+                    </Card>
+                  ))}
+                </form>
+              </Box>
+            </Grid>
+            <Hidden mdDown>
+              <Grid item xs={12} md={4}>
+                <Card>
+                  <CardContent>
+                    <Box>
+                      <Typography variant='body2' sx={{ fontWeight: 'bold' }}>
+                        Information describe interest survey
+                      </Typography>
+                      <Typography variant='body2'>
+                        There are many variations of passages of Lorem Ipsum available, but the majority have suffered
+                        alteration in some form, by injected humour, or randomised words which don't look even slightly
+                        believable.
+                      </Typography>
+                    </Box>
+                    <Box sx={{ marginTop: 4 }}>
+                      <Typography variant='body2' sx={{ fontWeight: 'bold' }}>
+                        Information describe study plan system
+                      </Typography>
+                      <Typography variant='body2'>
+                        There are many variations of passages of Lorem Ipsum available, but the majority have suffered
+                        alteration in some form, by injected humour, or randomised words which don't look even slightly
+                        believable.
+                      </Typography>
+                    </Box>
+                    <Box sx={{ marginTop: 4 }}>
+                      <Card
+                        sx={{
+                          backgroundColor: '#EBEBEB',
+                          height: '140px',
+                          display: 'flex',
+                          justifyContent: 'center',
+                          alignItems: 'center',
+                          color: '#7A7A7A'
+                        }}
+                      >
+                        Shortcut CE.Reform
+                      </Card>
+                    </Box>
+                    <Box sx={{ marginTop: 4 }}>
+                      <Card
+                        sx={{
+                          backgroundColor: '#EBEBEB',
+                          height: '140px',
+                          display: 'flex',
+                          justifyContent: 'center',
+                          alignItems: 'center',
+                          color: '#7A7A7A'
+                        }}
+                      >
+                        Shortcut CE.Reform
+                      </Card>
+                    </Box>
+                  </CardContent>
+                </Card>
+              </Grid>
+            </Hidden>
+          </Grid>
+        </Grid>
+        <Grid item xs={12}>
+          <Box sx={{ display: 'flex', justifyContent: 'flex-end', marginY: 4 }}>
+            <Button variant='text' size='large' sx={{ marginRight: 4, color: '#AAA' }}>
+              Reset
+            </Button>
+            <Button
+              variant='contained'
+              size='large'
+              type='submit'
+              sx={{ backgroundColor: '#EBEBEB', color: '#7A7A7A' }}
+            >
+              Submit
+            </Button>
+          </Box>
+        </Grid>
+      </Grid>
+    </Box>
+  )
+}
+
+export default InterestForm
---
+++ src/views/InterestSurvey/test.js
@@ -0,0 +1,193 @@
+// ** react imports
+import React, { useEffect, useState } from 'react'
+
+// ** mui components
+import {
+  Box,
+  Button,
+  Card,
+  FormControl,
+  FormControlLabel,
+  Radio,
+  RadioGroup,
+  Typography,
+  CircularProgress
+} from '@mui/material'
+
+// ** axios
+import axios from 'axios'
+
+const InterestForm = ({ dataSurvey, collegianCode, setAlertAnswer, setResultDisplay }) => {
+  const Questions = dataSurvey[0].interestQuestions
+  const [isLoading, setIsLoading] = useState(false)
+
+  const [answers, setAnswers] = useState(
+    Questions.map(question => ({
+      collegian_code: collegianCode,
+      question_type: question.interest_question_type,
+      interest_answer_id: null,
+      interest_question_score: null
+    }))
+  )
+
+  const handleInputChange = (event, index, type, answerId) => {
+    if (type === 1) {
+      console.log('index: ', index)
+      console.log('answerId: ', answerId)
+      console.log('value: ', event.target.value)
+
+      // const answerId = answerId[0].interest_answer_id
+      const value = parseInt(event.target.value, 10)
+      setAnswers(prevAnswers => {
+        const newAnswers = [...prevAnswers]
+        newAnswers[index] = {
+          ...newAnswers[index],
+          interest_question_score: value,
+          interest_answer_id: answerId
+        }
+
+        return newAnswers
+      })
+    } else if (type === 2) {
+      const value = parseInt(event.target.value, 10)
+      setAnswers(prevAnswers => {
+        const newAnswers = [...prevAnswers]
+        newAnswers[index] = { ...newAnswers[index], interest_answer_id: value }
+
+        return newAnswers
+      })
+    }
+  }
+
+  const handleSubmit = async event => {
+    event.preventDefault()
+
+    // ตรวจสอบเงื่อนไข
+    if (
+      answers.some(answer => {
+        if (answer.question_type === 1) {
+          return answer.interest_question_score === null
+        } else if (answer.question_type === 2) {
+          return answer.interest_answer_id === null
+        }
+
+        return false // ในกรณีที่ไม่ใช่ question_type ที่กำหนด
+      })
+    ) {
+      setAlertAnswer(true)
+    } else {
+      setIsLoading(true)
+
+      const payload = {
+        interest_records: answers
+      }
+
+      try {
+        // ทำการ POST ไปยัง API
+        const res = await axios.post(`${process.env.NEXT_PUBLIC_API_URL}interest-records`, payload, {
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        })
+
+        console.log(res)
+        setResultDisplay(true)
+      } catch (err) {
+        console.log(err)
+        setResultDisplay(false)
+      }
+
+      console.log(answers)
+    }
+  }
+
+  useEffect(() => {
+    console.log('answers: ', answers)
+  }, [answers])
+
+  if (isLoading) {
+    return (
+      <Box sx={{ display: 'flex', justifyContent: 'center' }}>
+        <CircularProgress />
+      </Box>
+    )
+  }
+
+  return (
+    <Box>
+      <Box
+        sx={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignContent: 'center',
+          p: 4
+        }}
+      >
+        <Typography variant='h3' component='h4' sx={{ fontWeight: 'bold' }}>
+          แบบประเมินตำแหน่งตามความสนใจ
+        </Typography>
+      </Box>
+      <Box sx={{ p: 4 }}>
+        <form onSubmit={handleSubmit}>
+          <Box sx={{ display: 'flex', flexDirection: 'column', p: 2 }}>
+            {Questions.map((question, index) => (
+              <Card key={index} sx={{ marginBlock: 2, p: 4 }}>
+                <Typography variant='h5' component='h6' sx={{ fontWeight: 'bold' }}>
+                  ข้อที่ {index + 1} {question.interest_question_title}
+                </Typography>
+                <FormControl key={index} sx={{ marginBlock: 2 }}>
+                  {question.interest_question_type === 1 ? (
+                    <RadioGroup
+                      row
+                      value={answers[index].interest_question_score}
+                      onChange={event =>
+                        handleInputChange(event, index, 1, question.interest_answers[0].interest_answer_id)
+                      }
+                    >
+                      {[1, 2, 3, 4, 5].map(answer => (
+                        <FormControlLabel
+                          key={answer}
+                          value={answer}
+                          control={<Radio />}
+                          label={<Typography variant='body1'>{answer}</Typography>}
+                          sx={{ marginBlock: '0.5rem' }}
+                        />
+                      ))}
+                    </RadioGroup>
+                  ) : (
+                    <RadioGroup
+                      aria-labelledby='demo-controlled-radio-buttons-group'
+                      name='controlled-radio-buttons-group'
+                      value={answers[index].interest_answer_id}
+                      onChange={event => handleInputChange(event, index, 2)}
+                    >
+                      {question.interest_answers.map((answer, index) => (
+                        <FormControlLabel
+                          key={answer.interest_answer_id}
+                          value={answer.interest_answer_id}
+                          control={<Radio />}
+                          label={<Typography variant='body1'>{answer.interest_answer_title}</Typography>}
+                          sx={{ marginBlock: '0.5rem' }}
+                        />
+                      ))}
+                    </RadioGroup>
+                  )}
+                </FormControl>
+              </Card>
+            ))}
+          </Box>
+          <Box sx={{ display: 'flex', justifyContent: 'end', p: 4 }}>
+            <Button variant='contained' color='error' size='large' sx={{ marginInline: 1 }}>
+              รีเซ็ต
+            </Button>
+            <Button variant='contained' color='success' size='large' sx={{ marginInline: 1 }} type='submit'>
+              ส่งแบบสอบถาม
+            </Button>
+          </Box>
+        </form>
+      </Box>
+    </Box>
+  )
+}
+
+export default InterestForm
